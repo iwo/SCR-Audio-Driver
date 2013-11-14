57029d6979084b2cd56369884344aab88330a791
Iwo Banas <banas.iwo@gmail.com>
2013-11-14 12:46:35 +0100

    define SCR_SDK_VERSION

--- Android.mk
+++ Android.mk
@@ -2,6 +2,7 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
+LOCAL_CFLAGS := -DSCR_SDK_VERSION=$(PLATFORM_SDK_VERSION)
 LOCAL_MODULE := audio.scr_primary.default
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_SRC_FILES := audio_hw.c
